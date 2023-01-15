259a2144dbe2799cb2409f164f903bde73230045
Alexander Veshkin <a.veshkin@avtopass.net>
2023-01-15 12:21:41 +0400

    wrked before move to src

---
+++ public/js/helper.js
@@ -0,0 +1,25 @@
+const el = (loc) => document.querySelector(loc);
+const els = (loc) => document.querySelectorAll(loc);
+
+async function fetchData(url = '', data = {}, method) {
+  const response = await fetch(url, {
+    method: method,
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify(data),
+  });
+  return response.json();
+}
+
+async function fetchDataGet(url = '') {
+  const response = await fetch(url);
+  return response.json();
+}
+
+export { el, els, fetchData, fetchDataGet };
